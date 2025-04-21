addad254d208be7a5e1ac0df6b9331bffe884e43
zehraa akça <zhrakcc00@gmail.com>
2025-04-21 10:37:36 +0300

    Ödev tamamlandı

---
+++ BookStore/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}
